db02bfceee6475c15dbb58c7189460680b3ac518
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-04-21 18:43:18 +0530

    Events

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-console.clear();
-const log = require("./logger");
-
 //to keep the  terminal clear
+console.clear();
 
-log("Hello there");
+//Initializtions
+const bToGb = Math.pow(10, 9);
+
+// const log = require("./logger");
+
+// //Path
+// const path = require("path");
+// const pathObj = path.parse(__filename);
+// console.log(pathObj.ext === ".js" ? "Javascript" : "Not javascript");
+
+// //OS
+// const OS = require("os");
+// console.log(OS.hostname().includes("lenovo") ? "This is Lenovo" : "Not lenovo");
+// console.log("Free Memory in Gb-", OS.freemem() / bToGb);
+// console.log("Total Memory in Gb-", OS.totalmem() / bToGb);
+
+// //File System
+// const fs = require("node:fs");
+// // console.log(fs.readdirSync("./"));
+// fs.readdir("./", function (err, files) {
+//   if (err) console.log("Error ", err);
+//   else console.log("Files ", files);
+// });
+
+const EventEmitter = require("events");
+const emmiter = new EventEmitter();
+
+emmiter.on("Event", function () {
+  console.log("Hello there");
+});
+
+emmiter.emit("Event");
--- logger.js
+++ logger.js
@@ -1,7 +1,15 @@
 var url = "http://mylogger.io/log";
 
-console.log("File", __filename);
-console.log("Dir", __dirname);
+// console.log("File", __filename);
+// console.log("Dir", __dirname);
+// console.log("Mod", module);
+console.table([
+  { a: 1, b: "Y" },
+  { a: "Z", b: 2 },
+  { a: "Z", b: 5 },
+  { a: "Z" },
+  { b: 5 },
+]);
 
 function log(message) {
   console.log(message);

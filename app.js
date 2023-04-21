9a760841dfa0d9f96c707465ddbbf51593f1c00f
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-04-21 11:39:51 +0530

    day's  initial commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
+console.clear();
 const log = require("./logger");
 
 //to keep the  terminal clear
-console.clear();
-// logger = 1;
 
 log("Hello there");
--- logger.js
+++ logger.js
@@ -1,5 +1,8 @@
 var url = "http://mylogger.io/log";
 
+console.log("File", __filename);
+console.log("Dir", __dirname);
+
 function log(message) {
   console.log(message);
 }
